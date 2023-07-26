68c04c19efbc61966b43050e09965ac0c8798d5e
Maciej Wyrzykowski <maciej_tbg@wp.pl>
2023-07-26 22:23:45 +0200

    Modified setting Google Calendar event according to params.

--- src/main/java/com/wedding/invitation/controllers/HomeController.java
+++ src/main/java/com/wedding/invitation/controllers/HomeController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -18,16 +19,38 @@ public class HomeController {
 
         String groomName = "Józef";
         String brideName = "Maria";
+        String weddingLocation = "Tarnobrzeg";
+        String eventSubject = brideName+"%20&%20"+groomName;
+        String eventDescription = "Najlepszy ślub na świecie!";
+        eventDescription = eventDescription.replace(" ","%20");
+
+
+
 
         //TODO {Zaimplementować pobieranie czasu z bazy danych}.
         Calendar calendar = Calendar.getInstance();
 
 //        calendar.set(2023, Calendar.AUGUST, 26, 12, 0, 0);
         calendar.add(Calendar.HOUR_OF_DAY,1);
-        Date weddingDate = calendar.getTime();
-        model.addAttribute("weddingDate",weddingDate);
+        Date weddingStartDate = calendar.getTime();
+        int weddingDuration = 12;
+        calendar.add(Calendar.HOUR,weddingDuration);
+        Date weddingEndDate = calendar.getTime();
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd'T'HHmmss");
+        String formattedWeddingStartDate = dateFormat.format(weddingStartDate);
+        String formattedWeddingEndDate = dateFormat.format(weddingEndDate);
+
+        String saveDateLink = "https://calendar.google.com/calendar/render?action=TEMPLATE&text="+eventSubject+"&details="+eventDescription+"%20text&dates="+formattedWeddingStartDate+"/"+formattedWeddingEndDate+"&location="+weddingLocation;
+
+
+
+
+        model.addAttribute("weddingStartDate",weddingStartDate);
+        model.addAttribute("weddingLocation",weddingLocation);
         model.addAttribute("groomName",groomName);
         model.addAttribute("brideName",brideName);
+        model.addAttribute("saveDateLink",saveDateLink);
         return "index";
     }
 }
